c9947276d32a2879ebbb976392d6d4ad2d054c67
Yhoundeh <aarond96@byui.edu>
2021-02-22 10:22:23 -0700

    AD--Problem in template.js?

--- js/template.js
+++ js/template.js
@@ -1,26 +1,11 @@
-/*fetch("./templates/header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
-
-fetch("./templates/footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
-  });*/
-
-  export function renderWithTemplate(template, parent, data, callback) {
+export function renderWithTemplate(template, parent, data, callback) {
     let clone = template.content.cloneNode(true);
     if(callback) {
       clone = callback(clone, data);
     }
     parent.appendChild(clone);
   }
+  
   export async function loadTemplate(path) {
     const html = await fetch(path).then(convertToText);
     const template = document.createElement('template');
@@ -29,10 +14,10 @@ fetch("./templates/footer.html")
   }
 
   export async function loadHeaderFooter() {
-    const header = await loadTemplate('./templates/header.html');
-    const footer = await loadTemplate('./templates/footer.html');
-    const headerElement = document.querySelector('header');
-    const footerElement = document.querySelector('footer');
+    const header = await loadTemplate('../templates/header.html');
+    const footer = await loadTemplate('../templates/footer.html');
+    const headerElement = document.getElementById('main-header');
+    const footerElement = document.getElementById('main-footer');
     renderWithTemplate(header, headerElement);
     renderWithTemplate(footer, footerElement);
   }
