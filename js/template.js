7c9d079748f1a2c0ec83c81cc414cd99331c99b2
Yhoundeh <aarond96@byui.edu>
2021-02-22 12:19:45 -0700

    AD revert merge error

--- js/template.js
+++ js/template.js
@@ -1,15 +1,23 @@
-fetch("./templates/header.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("header").innerHTML = data;
-  });
 
-fetch("./templates/footer.html")
-  .then(response => {
-    return response.text()
-  })
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
-  });
+export async function loadHeaderFooter() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("main-header").innerHTML =
+      this.responseText;
+    }
+  };
+
+  xhttp.open("GET", "header.txt", true);
+  xhttp.send();
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("main-footer").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "footer.txt", true);
+  xhttp.send();
+}
