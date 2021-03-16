97bf6d778f5a3562d465f44bd3d3d08233f5e3cf
Yhoundeh <aarond96@byui.edu>
2021-03-16 12:16:05 -0600

    -- Ressurected!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-import {loadHeaderFooter, loadPosts, loadSidebar} from './template.js';
+import {loadHeaderFooter, loadSidebar} from './utils.js';
+import postList from './postList.js';
+import {getModal} from './modal.js';
+
+const post = new postList(document.querySelector('.post-list'));
 
 loadHeaderFooter();
 loadSidebar();
-loadPosts();
+
+post.init();
+
+const img = document.querySelectorAll('.post-card__img');
+img.forEach(function(openBtn){
+    openBtn.addEventListener("click", getModal);
+})
+const span = document.querySelectorAll(".close");
+span.forEach(function(closeBtn){
+    closeBtn.addEventListener('click', getModal)
+})
